03abac8c8e63023c8aa5171881c312d39d6b03a9
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-25 07:01:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,10 +9,6 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 def won?(board)
   status= false
   WIN_COMBINATIONS.each do |win_array|
@@ -38,4 +34,50 @@ end
 def winner(board)
   won = won?(board)
   !over?(board) || !won?(board) ? nil : board[won[0]]
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  9.times do
+    turn(board)
+  end
 end
