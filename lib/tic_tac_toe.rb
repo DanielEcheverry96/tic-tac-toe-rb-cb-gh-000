04c90a1f8613397ea80ff84a625772b56129e43e
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-25 07:05:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,8 +52,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
+def move(board, index, caracter = "X") #Realiza el movimiento en el tablero
+  board[index] = caracter
 end
 
 def position_taken?(board, location)
