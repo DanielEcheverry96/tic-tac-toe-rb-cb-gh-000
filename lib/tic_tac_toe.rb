328ff43be8d435acc7d636f9a945c4693f1b8273
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-25 07:03:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,6 +76,20 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
 def play(board)
   9.times do
     turn(board)
